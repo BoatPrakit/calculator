d79087b77af30f468246491e9bdc9f93f916270e
BoatPrakit <boatnakrub_@hotmail.com>
2020-09-07 00:11:36 +0700

    [Start]Calculator

--- src/index.js
+++ src/index.js
@@ -1,17 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function Button(props){
+  return (
+  <button className="button" onClick={props.onClick}> {props.value}</button>
+  )
+}
+class Screen extends React.Component{
+  render(){
+    return (
+      <div className="screen">
+        {this.props.value}
+      </div>
+    )
+  }
+}
+class ButtonPad extends React.Component{
+  renderButton (value) {
+    return (
+      <Button 
+         value={value}
+         onClick={()=>this.props.onClick(value)}
+      />
+    )
+  }
+  render(){
+    return (
+      <div className="button-pad">
+        <div className="col-1">
+          <div className="operator-row">
+            {this.renderButton("X")}
+            {this.renderButton("÷")}
+            {this.renderButton("C")}
+            {this.renderButton("<<")}
+          </div>
+          <div className="row-1">
+            {this.renderButton("7")}
+            {this.renderButton("8")}
+            {this.renderButton("9")}
+            {this.renderButton("-")}
+          </div>
+          <div className="row-2">
+            {this.renderButton("4")}
+            {this.renderButton("5")}
+            {this.renderButton("6")}
+            {this.renderButton("+")}
+          </div>
+          <div className="row-3">
+            {this.renderButton("1")}
+            {this.renderButton("2")}    
+            {this.renderButton("3")}
+            {this.renderButton("=")}
+          </div>
+        </div>
+          <div className="col-2">
+          </div>
+      </div>
+    )
+  }
+}
+class Calculator extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      beforeCalculate : '',
+      isClear : false
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(value){
+    this.setState({
+      beforeCalculate : this.state.beforeCalculate +value
+    })
+  }
+  render(){
+    return (
+      <div className="calculator">
+          <Screen value={this.state.beforeCalculate} />
+          <ButtonPad onClick={this.handleClick}/>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Calculator />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
